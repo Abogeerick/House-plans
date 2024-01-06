3f82e95aedee95510c0340e33407cd0118dac280
Abogeerick <abogeerick@gmail.com>
2024-01-06 12:35:25 +0300

    Created a Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+// Import your components
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
