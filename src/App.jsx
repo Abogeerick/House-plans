fcbe97776633a54d85bbb64e42b40c3d4b4293c7
Abogeerick <abogeerick@gmail.com>
2024-01-06 13:30:03 +0300

    Created contactus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
+import ContactUs from './ContactUs';
 // Import your components
 
 const App = () => {
@@ -8,10 +9,10 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} /> */}
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="/blog" element={<Blog />} />
+        {/* <Route path="/blog" element={<Blog />} /> */}
         {/* Add more routes as needed */}
       </Routes>
     </Router>
